7a0da536340a595b7419fd4e030d6aa774450004
wheaton5 <whheaton@gmail.com>
2021-12-02 14:54:34 -0600

    arbitrary calculation

--- src/main.rs
+++ src/main.rs
@@ -94,6 +94,9 @@ fn _main() -> Result<(), Error> {
 
 fn phase_chunk<'a>(data: &ThreadData) -> Result<(), Error> {
     println!("thread {} chrom {}", data.index, data.chrom);
+    for i in 0..1000000 {
+        eprintln!("{}", i);
+    }
     let long_read_bam_reader = match &data.long_read_bam {
         Some(x) => Some(bam::IndexedReader::from_path(x)?),
         None => None,
