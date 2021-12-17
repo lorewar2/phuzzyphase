26658ab6aba3b19189aba49ea5405e467327a899
wheaton5 <whheaton@gmail.com>
2021-12-17 15:13:31 -0600

    read assignment via alignment

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,20 @@ use failure::{Error, ResultExt};
 use flate2::read::MultiGzDecoder;
 use rayon::prelude::*;
 use rust_htslib::bam::{self, Read, Record};
+use rust_htslib::bam::ext::BamRecordExtensions;
 use rust_htslib::bcf::{self, Read as BcfRead};
 use std::path::Path;
 
 use hashbrown::{HashMap, HashSet};
 
 use clap::App;
+
+const K: usize = 6; // kmer match length
+const W: usize = 20; // Window size for creating the band
+const MATCH: i32 = 1; // Match score
+const MISMATCH: i32 = -5; // Mismatch score
+const GAP_OPEN: i32 = -5; // Gap open score
+const GAP_EXTEND: i32 = -1; // Gap extend score
 
 fn main() {
     let res = _main();
@@ -49,8 +57,10 @@ fn _main() -> Result<(), Error> {
         .expect(&format!("error opening fasta index: {}", fai))
         .sequences();
     let mut chroms: Vec<String> = Vec::new();
+    let mut chrom_lengths: Vec<u64> = Vec::new();
     for chrom in fa_index_iter {
         chroms.push(chrom.name.to_string());
+        chrom_lengths.push(chrom.len);
     }
 
     let mut chunks: Vec<ThreadData> = Vec::new();
@@ -72,6 +82,10 @@ fn _main() -> Result<(), Error> {
             fasta: params.fasta.to_string(),
             chrom: chrom.to_string(),
             vcf: params.vcf.to_string(),
+            min_mapq: params.min_mapq,
+            min_base_qual: params.min_base_qual,
+            chrom_length: chrom_lengths[i],
+            window: params.window,
         };
         chunks.push(data);
     }
@@ -95,19 +109,35 @@ fn phase_chunk(data: &ThreadData) -> Result<(), Error> {
 }
 
 fn get_molecule_alleles_assignments(data: &ThreadData) -> Result<MoleculeAllelesWrapper, Error> {
-    let long_read_bam_reader = match &data.long_read_bam {
+    let mut long_read_bam_reader = match &data.long_read_bam {
         Some(x) => Some(bam::IndexedReader::from_path(x)?),
         None => None,
     };
-    let linked_read_bam_reader = match &data.linked_read_bam {
+    let mut linked_read_bam_reader = match &data.linked_read_bam {
         Some(x) => Some(bam::IndexedReader::from_path(x)?),
         None => None,
     };
-    let hic_bam_reader = match &data.hic_bam {
+    let mut hic_bam_reader = match &data.hic_bam {
         Some(x) => Some(bam::IndexedReader::from_path(x)?),
         None => None,
     };
+    let mut fasta = fasta::IndexedReader::from_file(&data.fasta).expect("cannot open fasta file");
 
+    let mut molecule_alleles: MoleculeAlleleWrapper = MoleculeAlleleWrapper{
+        long_read_assignments: match &data.long_read_bam {
+            Some(_x) => Some(HashMap::new()),
+            None => None,
+        },
+        linked_read_assignments: match &data.linked_read_bam {
+            Some(_x) => Some(HashMap::new()),
+            None => None,
+        },
+        
+        hic_read_assignments: match &data.hic_bam {
+            Some(_x) => Some(HashMap::new()),
+            None => None,
+        },
+    };
    
 
     let mut vcf_reader = bcf::IndexedReader::from_path(data.vcf.to_string())?;
@@ -119,20 +149,32 @@ fn get_molecule_alleles_assignments(data: &ThreadData) -> Result<MoleculeAlleles
         total += 1;
         let rec = _rec?;
         let pos = rec.pos();
-        let rec_rid = rec.rid().expect("could not unwrap vcf record id");
-        if rec_rid != rid {
-            println!("vcf read off end of chromosome {} to {:?}", data.chrom, 
-                std::str::from_utf8(vcf_reader.header().rid2name(rec_rid)?));
-            break;
+        let alleles = rec.alleles();
+        if alleles.len() > 2 {
+            continue; // ignore multi allelic sites
         }
+        let reference = std::str::from_utf8(alleles[0])?;
+        let alternative = std::str::from_utf8(alleles[1])?;
+        let rec_rid = rec.rid().expect("could not unwrap vcf record id");
+
         let genotypes = rec.genotypes()?;
         let genotype = genotypes.get(0); // assume only 1 and get the first one
         if is_heterozygous(genotype) {
             hets += 1;
-            let molecule_assignments = get_molecule_allele_assignments(
-                &long_read_bam_reader,
-                &linked_read_bam_reader,
-                &hic_bam_reader,
+            get_molecule_allele_assignments(
+                &data.chrom,
+                pos as usize,
+                reference.to_string(),
+                alternative.to_string(),
+                data.min_mapq,
+                data.min_base_qual,
+                &mut long_read_bam_reader,
+                &mut linked_read_bam_reader,
+                &mut hic_bam_reader,
+                &mut fasta,
+                data.window,
+                data.chrom_length,
+                &mut molecule_alleles,
             );
         }
     }
@@ -144,16 +186,121 @@ fn get_molecule_alleles_assignments(data: &ThreadData) -> Result<MoleculeAlleles
     })
 }
 
-fn get_molecule_allele_assignments(
-    long_reads: &Option<bam::IndexedReader>,
-    linked_reads: &Option<bam::IndexedReader>,
-    hic_reads: &Option<bam::IndexedReader>,
-) -> MoleculeAlleleWrapper {
 
-    MoleculeAlleleWrapper{
-        long_read_assignments: None,
-        linked_read_assignments: None,
-        hic_read_assignments: None,
+fn get_molecule_allele_assignments(
+    chrom: &String,
+    pos: usize,
+    ref_allele: String,
+    alt_allele: String,
+    min_mapq: u8,
+    min_base_qual: u8,
+    long_reads: &mut Option<bam::IndexedReader>,
+    linked_reads: &mut Option<bam::IndexedReader>,
+    hic_reads: &mut Option<bam::IndexedReader>,
+    fasta: &mut fasta::IndexedReader<std::fs::File>,
+    window: usize,
+    chrom_length: u64,
+    molecule_alleles: &mut MoleculeAlleleWrapper,
+) {
+    if (pos + window) as u64 > chrom_length {
+        return;
+    }
+    match long_reads {
+        Some(bam) => {
+            let tid = bam.header().tid(chrom.as_bytes()).expect("cannot find chrom tid");
+            bam.fetch((tid, pos as u32, (pos + 1) as u32)).expect("blah");
+            let ref_start = (pos - window) as u64;
+            let ref_end = (pos + window + ref_allele.len()) as u64;
+            fasta.fetch(chrom, ref_start, ref_end);
+            let mut ref_sequence: Vec<u8> = Vec::new();
+            fasta.read(&mut ref_sequence).expect("failed to read fasta sequence");
+
+            let mut alt_sequence: Vec<u8> = Vec::new();
+            for i in 0..window as usize {
+                alt_sequence.push(ref_sequence[i]);
+            }
+            for base in alt_allele.as_bytes() {
+                alt_sequence.push(*base);
+            }
+            for i in (window as usize+ref_allele.len())..ref_sequence.len() {
+                alt_sequence.push(ref_sequence[i]);
+            }
+            
+            println!("double checking. ref allele {} alt {} chrom {} pos {}", ref_allele, alt_allele, chrom, pos);
+            println!("ref_sequence {}", std::str::from_utf8(&ref_sequence).unwrap());
+            println!("alt_sequence {}", std::str::from_utf8(&alt_sequence).unwrap());
+            println!("");
+            for _rec in bam.records() {
+                let rec = _rec.expect("cannot read bam record");
+                println!("\n{}", std::str::from_utf8(rec.qname()).unwrap());
+                if rec.mapq() < min_mapq {
+                    continue
+                }
+                if rec.is_secondary() || rec.is_supplementary() {
+                    continue;
+                }
+                let mut read_start: usize = 10000000000; // I am lazy and for some reason dont know how to do things, so this is my bad solution
+                let mut read_end: usize = rec.seq_len();
+                let mut min_bq = 93;
+                let qual = rec.qual();
+                for pos_pair in rec.aligned_pairs() {
+                    if (pos_pair[1] as u64) >= ref_start && (pos_pair[1] as u64) < ref_end {
+                        if pos_pair[1] as usize >= pos && pos_pair[1] as usize <= pos + ref_allele.len().max(alt_allele.len()) {
+                            min_bq = min_bq.min(qual[pos_pair[0] as usize]);
+                        }
+                        read_end = pos_pair[0] as usize;
+                        if read_start == 10000000000 {
+                            read_start = pos_pair[0] as usize;
+                        }
+                    } 
+                }
+                if min_bq < min_base_qual {
+                    continue;
+                }
+                if read_start >= read_end {
+                    eprintln!("what happened, read start {} read end {}",read_start, read_end);
+                    continue;
+                }
+                let seq = rec.seq().encoded[read_start..read_end].to_vec();
+                eprintln!("ref sequence {}", std::str::from_utf8(&ref_sequence).unwrap());
+                eprintln!("alt sequence {}", std::str::from_utf8(&alt_sequence).unwrap());
+                eprintln!("read segment {}\n", std::str::from_utf8(&seq).unwrap());
+                let score = |a: u8, b: u8| if a == b { MATCH } else { MISMATCH };
+                let mut aligner = banded::Aligner::new(GAP_OPEN, GAP_EXTEND, score, K, W);
+                let ref_alignment = aligner.local(&seq, &ref_sequence);
+                let alt_alignment = aligner.local(&seq, &alt_sequence);
+                if ref_alignment.score > alt_alignment.score {
+                    eprintln!("read supports ref allele");
+                    match &mut molecule_alleles.long_read_assignments {
+                        Some(long_read_assignment) => {
+                            let readdata = long_read_assignment.entry(std::str::from_utf8(rec.qname()).expect("readname fail").to_string()).or_insert(Vec::new());
+                            readdata.push(Allele{
+                                locus: pos,
+                                allele: true,
+                            });
+                        },
+                        None => (),
+                    }
+                } else if alt_alignment.score > ref_alignment.score {
+                    eprintln!("read supports alt allele");
+                    match &mut molecule_alleles.long_read_assignments {
+                        Some(long_read_assignment) => {
+                            let readdata = long_read_assignment.entry(std::str::from_utf8(rec.qname()).expect("readname fail").to_string()).or_insert(Vec::new());
+                            readdata.push(Allele{
+                                locus: pos,
+                                allele: false,
+                            });
+                        },
+                        None => (),
+                    }
+                } else {
+                    eprintln!("\nread had equal alignment scores ref {} alt {}", ref_allele, alt_allele);
+                    
+                }
+                
+            }
+        }
+        None => (),
     }
 }
 
@@ -178,20 +325,19 @@ fn is_heterozygous(gt: bcf::record::Genotype) -> bool {
     return false;
 }
 
+
+
 struct MoleculeAlleleWrapper {
-    long_read_assignments: Option<ReadAssignments>,
-    linked_read_assignments: Option<ReadAssignments>,
-    hic_read_assignments: Option<ReadAssignments>,
+    long_read_assignments: Option<HashMap<String, Vec<Allele>>>,
+    linked_read_assignments: Option<HashMap<String, Vec<Allele>>>,
+    hic_read_assignments: Option<HashMap<String, Vec<Allele>>>,
 }
 
-struct ReadAssignments {
-    assignments: Vec<ReadAssignment>,
+struct Allele {
+    locus: usize,
+    allele: bool, // ref is 0 and alt is 1
 }
 
-struct ReadAssignment {
-    id: String,
-    allele: bool,
-}
 
 
 struct MoleculeAllelesWrapper {
@@ -209,7 +355,11 @@ struct ThreadData {
     hic_bam: Option<String>,
     fasta: String,
     chrom: String,
+    chrom_length: u64,
     vcf: String,
+    min_mapq: u8,
+    min_base_qual: u8,
+    window: usize,
 }
 
 #[derive(Clone)]
@@ -224,6 +374,9 @@ struct Params {
     fasta: String,
     ploidy: usize,
     vcf: String,
+    min_mapq: u8,
+    min_base_qual: u8,
+    window: usize,
 }
 
 fn load_params() -> Params {
@@ -250,6 +403,12 @@ fn load_params() -> Params {
     let seed = params.value_of("seed").unwrap_or("4"); // 4 is guarranteed random by dice roll https://xkcd.com/221/
     let seed = seed.to_string().parse::<u8>().unwrap();
 
+    let min_mapq = params.value_of("min_mapq").unwrap_or("30");
+    let min_mapq = min_mapq.to_string().parse::<u8>().unwrap();
+    
+    let min_base_qual = params.value_of("min_base_qual").unwrap_or("20");
+    let min_base_qual = min_base_qual.to_string().parse::<u8>().unwrap();
+
     let restarts = params.value_of("restarts").unwrap_or("10");
     let restarts = restarts.to_string().parse::<u32>().unwrap();
 
@@ -257,6 +416,9 @@ fn load_params() -> Params {
 
     let ploidy = params.value_of("ploidy").unwrap_or("2");
     let ploidy = ploidy.to_string().parse::<usize>().unwrap();
+
+    let allele_alignment_window = params.value_of("ploidy").unwrap_or("200");
+    let allele_alignment_window = allele_alignment_window.to_string().parse::<usize>().unwrap();
 
     let vcf = params.value_of("vcf").unwrap();
 
@@ -271,5 +433,8 @@ fn load_params() -> Params {
         ploidy: ploidy,
         vcf: vcf.to_string(),
         restarts: restarts,
+        min_mapq: min_mapq,
+        min_base_qual: min_base_qual,
+        window: allele_alignment_window,
     }
 }
